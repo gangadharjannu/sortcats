27e33c32b229f1ba93f5032292de125e1b6872fc
gangadharjannu <gangadhar014@gmail.com>
2017-04-04 02:11:59 +0530

    Added new component 'pet'

---
+++ src/app/pets/pets.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { PetsService } from '../services/pets.service';
+
+import { Pets } from '../services/Pets';
+@Component({
+  selector: 'app-pets',
+  templateUrl: './pets.component.html',
+  styleUrls: ['./pets.component.css']
+})
+export class PetsComponent implements OnInit {
+  @Input() gender: string;
+  @Input() pets: Pets;
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
