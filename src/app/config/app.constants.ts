268f779389bae0dfa03d8674ec10dc7b9ec51736
gangadharjannu <gangadhar014@gmail.com>
2017-04-04 02:12:15 +0530

    Added global configuration file

---
+++ src/app/config/app.constants.ts
@@ -0,0 +1,5 @@
+export class Constants {
+    public static API_ENDPOINT = 'agl-developer-test.azurewebsites.net/people.json';
+    public static API_ENDPOINT_PUBLIC = 'https://cors-anywhere.herokuapp.com/' + Constants.API_ENDPOINT;
+    // public static API_ENDPOINT_PUBLIC = 'http://crossorigin.me/' + Constants.API_ENDPOINT;
+}
